add31b6f4746343568f0900cd0a19ae55cde3c7b
Benoît du Garreau <bdgdlm@outlook.com>
2024-01-14 16:45:50 +0100

    Derive macro: add more checks

--- macros/src/derive.rs
+++ macros/src/derive.rs
@@ -34,6 +34,8 @@ impl Format {
 
 pub fn run(input: syn::DeriveInput) -> syn::Result<TokenStream> {
     let format = get_format(&input.attrs)?;
+    check_fields(&input.data)?;
+
     let loader = format.path();
     let ext = format.extensions();
 
@@ -67,16 +69,15 @@ fn add_clauses(gen: &mut syn::WhereClause, format: Format) {
     gen.predicates.push(trait_clause);
 }
 
+fn is_format_attribute(meta: &syn::Meta) -> bool {
+    meta.path().get_ident().is_some_and(|i| i == "asset_format")
+}
+
 fn get_format(attrs: &[syn::Attribute]) -> syn::Result<Format> {
     let mut formats = None;
 
     for attr in attrs {
-        if !attr
-            .meta
-            .path()
-            .get_ident()
-            .is_some_and(|i| i == "asset_format")
-        {
+        if !is_format_attribute(&attr.meta) {
             continue;
         }
 
@@ -108,4 +109,39 @@ fn get_format(attrs: &[syn::Attribute]) -> syn::Result<Format> {
     }
 
     formats.ok_or_else(|| syn::Error::new(Span::call_site(), "missing asset format"))
+}
+
+fn check_fields(data: &syn::Data) -> syn::Result<()> {
+    let check_attrs = |attrs: &[syn::Attribute]| {
+        for attr in attrs {
+            if is_format_attribute(&attr.meta) {
+                return Err(syn::Error::new_spanned(attr, "unexpected attribute"));
+            }
+        }
+        Ok(())
+    };
+
+    match data {
+        syn::Data::Struct(s) => {
+            for field in &s.fields {
+                check_attrs(&field.attrs)?;
+            }
+        }
+        syn::Data::Enum(e) => {
+            for variant in &e.variants {
+                check_attrs(&variant.attrs)?;
+
+                for field in &variant.fields {
+                    check_attrs(&field.attrs)?;
+                }
+            }
+        }
+        syn::Data::Union(u) => {
+            for field in &u.fields.named {
+                check_attrs(&field.attrs)?;
+            }
+        }
+    }
+
+    Ok(())
 }
